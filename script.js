48c23d934ce24236655875dab69e58ce8a3944ce
GuiceJoose <snowmanjz@gmail.com>
2022-03-22 13:31:10 -0500

    create working js and HTML

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 function computerPlay(){
     let computerHand = Math.floor(Math.random()*3);
     if (computerHand == 1){
@@ -9,4 +11,60 @@ function computerPlay(){
     }
 }
 
-console.log(computerPlay());
+
+function playRound(playerSelection, computerSelection){
+    
+    
+    if (playerSelection === 'rock'){
+        if (computerSelection === 'Rock') {
+            return 'It\'s a tie\!'
+        } else if (computerSelection === 'Scissors') {
+            return 'You win!'
+        } else { 
+            return 'You lose!';
+        }
+    } else if (playerSelection === 'scissors'){
+        if (computerSelection === 'Scissors') {
+            return 'It\'s a tie\!'
+        } else if (computerSelection === 'Paper') {
+            return 'You win!'
+        } else { 
+            return 'You lose!';
+        }
+    } 
+    if (playerSelection === 'paper'){
+        if (computerSelection === 'Paper') {
+            return 'It\'s a tie\!'
+        } else if (computerSelection === 'Rock') {
+            return 'You win!'
+        } else { 
+            return 'You lose!';
+        }
+    } else {
+        return 'That\'s not rock, paper, or scissors!';
+    }
+}
+
+function game(){
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++){
+        computerSelection = computerPlay();
+        playerSelection = prompt('Rock, paper, or scissors?','').toLowerCase();
+        console.log(playRound(playerSelection, computerSelection));
+        if (playRound(playerSelection, computerSelection) == 'You win!') {
+            playerScore++;
+        } else if (playRound(playerSelection, computerSelection) == 'You lose!') {
+            computerScore++;
+        }
+        console.log(playerScore, computerScore);
+    }
+    if (playerScore > computerScore) {
+        console.log('You are big winner!')
+    } else if (playerScore < computerScore) {
+        console.log('You are big loser!')
+    } else {
+        console.log('The game was a tie')
+    }
+}
+game();
